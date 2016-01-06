de4078cd01e4616f0cf59ee569aa32064b5e8422
Lewis Jang <lewis.aedes@gmail.com>
2016-01-06 15:20:28 +0000

    update joomla versoin

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'R4H5dxPZIRhwo04l2BXUzh5LEosYq6aY',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => '/var/www/sapience.com.tw/tmp/Joomla_3.4.8-Stable-Update_Package.zip',
+	'kickstart.setup.destdir' => '/var/www/sapience.com.tw',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
